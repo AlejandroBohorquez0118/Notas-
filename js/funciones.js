1e6947d5a224db7174a7b9bab5bfb8617befac34
Alejandro Bohorquez <diego.bohorquez.mt@correo.usa.edu.co>
2021-11-11 19:08:25 -0500

    visualizador de notas 2.0

--- js/funciones.js
+++ js/funciones.js
@@ -37,6 +37,86 @@ function consultar(){
                 }
                 $("#resultado").append(tabla + filas+"<tr><th colspan='2'>Promedio:<td>"+total +"</center>")
                 console.log(json)
+                console.log("porongaS")
+                console.log(json.items[0].nombre)
+
+                tablo =`<table class="table">
+            <thead>
+             <tr> 
+             
+             <b>`+json.items[0].nombre+`</b>
+             
+             </tr>
+              <tr class="table-dark">
+                <th scope="col">Nombre Nota</th>
+                <th scope="col">Resultado</th>
+                
+              </tr>
+            </thead>
+            <tbody>
+              
+            <tr class="table-light">
+            <th scope="row">Frontend_reto2</th>
+            <td>`+json.items[0].frontend_reto2+`</td>
+            
+            </tr>
+
+            <tr class="table-light">
+            <th scope="row">Frontend_reto3</th>
+            <td>`+json.items[0].frontend_reto3+`</td>
+            
+            </tr>
+
+            <tr class="table-light">
+            <th scope="row">Frontend_reto4</th>
+            <td>`+json.items[0].frontend_reto4+`</td>
+            
+            </tr>
+
+            <tr class="table-light">
+            <th scope="row">Frontend_retu5</th>
+            <td>`+json.items[0].frontend_reto5+`</td>
+            
+            </tr>
+
+            <tr class="table-light">
+            <th scope="row">Mensaje_r2</th>
+            <td>`+json.items[0].mensaje_r2+`</td>
+            
+            </tr>
+
+            <tr class="table-light">
+            <th scope="row">Mensaje_r3</th>
+            <td>`+json.items[0].mensaje_r3+`</td>
+            
+            </tr>
+
+            <tr class="table-light">
+            <th scope="row">Mensaje_r4</th>
+            <td>`+json.items[0].mensaje_r4+`</td>
+            
+            </tr>
+
+            <tr class="table-light">
+            <th scope="row">Mensaje_r5</th>
+            <td>`+json.items[0].mensaje_r5+`</td>
+            
+            </tr>
+
+            <tr class="table-light">
+            <th scope="row">Promedio</th>
+            <td>`+json.items[0].promedio+`</td>
+            
+            </tr>
+                
+                
+            </tbody>
+          </table>
+
+
+
+                `
+                $("#table").append(tablo)
             }
         });
 }
